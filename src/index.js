9d60ef4c3de44524a3e7a31f6d9aa3363283b723
mayur0211 <9dotmayur@gmail.com>
2024-08-14 17:49:24 +0530

    change in index file

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { SearchProvider } from './context';
+import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <SearchProvider>
-    <App />
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#94a3b8',
+          
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </SearchProvider>
 );
 reportWebVitals();
